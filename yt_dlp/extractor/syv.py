71998345b69ad65d82554d66cd3a74e0f7d2f7bc
HobbyistDev <tesutonihon4@gmail.com>
2022-06-28 23:15:24 +0900

    [24syv.dk] Add `description`, `episode_id`, change `.` to `\.` in `_VALID_URL`

--- yt_dlp/extractor/syv.py
+++ yt_dlp/extractor/syv.py
@@ -3,7 +3,7 @@ from ..utils import int_or_none, str_or_none, traverse_obj
 
 
 class SyvIE(InfoExtractor):
-    _VALID_URL = r'https?://24syv.dk/episode/(?P<id>[\w-]+)'
+    _VALID_URL = r'https?://24syv\.dk/episode/(?P<id>[\w-]+)'
     _TESTS = [{
         'url': 'https://24syv.dk/episode/isabella-arendt-stiller-op-for-de-konservative-2',
         'info_dict': {
@@ -12,8 +12,9 @@ class SyvIE(InfoExtractor):
             'title': 'Isabella Arendt stiller op for De Konservative',
             'display_id': 'isabella-arendt-stiller-op-for-de-konservative-2',
             'thumbnail': 'https://d1bm3dmew779uf.cloudfront.net/rss/show/5196630/4ceb292fd51c555feb652edfdf7291ed.png',
+            'description': 'md5:b9aefe6406345d5447b80d30a5f09aa3',
             'duration': 3218,
-            
+            'episode_id': '50183310',
         }
     }]
     
@@ -30,6 +31,8 @@ class SyvIE(InfoExtractor):
             'url': json_data['enclosure'],
             'display_id': json_data.get('slug') or video_id,
             'thumbnail': json_data.get('image_url'),
+            'description': json_data.get('post_title'),
             'duration': int_or_none(json_data.get('duration')),
+            'episode_id': json_data.get('episode_id'),
             
         }

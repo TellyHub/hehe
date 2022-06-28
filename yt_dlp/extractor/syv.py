56f60cc3f811cdf5001a1ddcc6447a200ae13b1d
HobbyistDev <tesutonihon4@gmail.com>
2022-06-28 23:03:30 +0900

    [24syv] Add `duration`, improve `json_data`, fix `_VALID_URL`, fix test

--- yt_dlp/extractor/syv.py
+++ yt_dlp/extractor/syv.py
@@ -1,30 +1,35 @@
 from .common import InfoExtractor
-from ..utils import str_or_none
+from ..utils import int_or_none, str_or_none, traverse_obj
 
 
 class SyvIE(InfoExtractor):
-    _VALID_URL = r'https?://24syv.dk/episode/<id>'
+    _VALID_URL = r'https?://24syv.dk/episode/(?P<id>[\w-]+)'
     _TESTS = [{
         'url': 'https://24syv.dk/episode/isabella-arendt-stiller-op-for-de-konservative-2',
         'info_dict': {
-            'id': 'fixme',
-            'ext': 'mp4',
-            'title': 'fixme',
+            'id': '12215',
+            'ext': 'mp3',
+            'title': 'Isabella Arendt stiller op for De Konservative',
+            'display_id': 'isabella-arendt-stiller-op-for-de-konservative-2',
+            'thumbnail': 'https://d1bm3dmew779uf.cloudfront.net/rss/show/5196630/4ceb292fd51c555feb652edfdf7291ed.png',
+            'duration': 3218,
+            
         }
     }]
     
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        json_data = self._search_nextjs_data(
-            self._download_webpage(url, video_id), video_id)['props']['pageProps']['episodeDetails']['details']
-            
-        
+        webpage = self._download_webpage(url, video_id)
+        nextjs_data = self._search_nextjs_data(webpage, video_id)
+        json_data = traverse_obj(
+            nextjs_data, ('props','pageProps', 'episodeDetails', ..., 'details'))[0]
+              
         return {
-            'id': str_or_none(json_data['id']),
+            'id': str_or_none(json_data['ID']),
             'title': json_data.get('post_content'),
             'url': json_data['enclosure'],
             'display_id': json_data.get('slug') or video_id,
             'thumbnail': json_data.get('image_url'),
-            'duration': json_data.get('duration'),
+            'duration': int_or_none(json_data.get('duration')),
             
         }
